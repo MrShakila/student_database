d4214b1d4cbfdea5bb2070aa9600275c917b1678
MrShakila <dscshakila@gmail.com>
2022-07-15 19:26:45 +0530

    StudentViewModel fixed

--- app/src/main/java/com/example/student_database/MainActivity.kt
+++ app/src/main/java/com/example/student_database/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.student_database
 
+import WordViewModel
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val newWordActivityRequestCode = 1
+    private val studentviewmodel: WordViewModel by viewModels {
+        Student((application as StudentApplications).repository)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +26,37 @@ class MainActivity : AppCompatActivity() {
         val adapter = StudentListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+        studentviewmodel.allWords.observe( this) { words ->
+            // Update the cached copy of the words in the adapter.
+            words.let { adapter.submitList(it) }
+        }
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewStdentActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intentData)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            intentData?.getStringExtra(NewStdentActivity.EXTRA_REPLY)?.let { reply ->
+                val word = StudentData(reply, subject1 =reply, subject2 = reply, subject3 = reply , id = taskId);
+                studentviewmodel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
